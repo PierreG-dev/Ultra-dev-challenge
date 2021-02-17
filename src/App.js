b10b7c76a4ec797481fa6e77d4d15d32ec4ed8e8
AniTecK <aniteck82@hotmail.fr>
2021-02-17 15:48:25 +0100

    yarn add eslint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useContext } from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import List from "./pages/List";
+import Films from "./pages/Films";
+import New from "./pages/New";
+import Series from "./pages/Series";
 
 function App() {
+  const [connected, setConnected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {connected ? (
+          <Switch>
+            <Route path={"/"}>
+              <Home />
+            </Route>
+
+            <Route path={"/Films"}>
+              <Films />
+            </Route>
+
+            <Route path={"/List"}>
+              <List />
+            </Route>
+
+            <Route path={"/New"}>
+              <New />
+            </Route>
+
+            <Route path={"/Series"}>
+              <Series />
+            </Route>
+
+            <Route>
+              <p>Error 404</p>
+            </Route>
+          </Switch>
+        ) : (
+          <Switch>
+            <Route path={"/"}>
+              <Login />
+            </Route>
+
+            <Route>
+              <p>Error 404</p>
+            </Route>
+          </Switch>
+        )}
+      </div>
+    </Router>
   );
 }
 
